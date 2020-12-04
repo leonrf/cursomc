5770e246580fd42b687778256ef627dad1ea1b5d
Leon Rossini <leonrf86@gmail.com>
2020-12-04 17:33:22 -0300

    Estado e Cidade

--- src/main/java/com/am/aguamarinha/CursomcApplication.java
+++ src/main/java/com/am/aguamarinha/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.am.aguamarinha.domain.Categoria;
+import com.am.aguamarinha.domain.Cidade;
+import com.am.aguamarinha.domain.Estado;
 import com.am.aguamarinha.domain.Produto;
 import com.am.aguamarinha.repositories.CategoriaRepository;
+import com.am.aguamarinha.repositories.CidadeRepository;
+import com.am.aguamarinha.repositories.EstadoRepository;
 import com.am.aguamarinha.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,11 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository; // Objeto que permite salvar e coletar inf do bando de dados
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -34,16 +42,31 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p1 = new Produto("Computador", 2000.00);
 		Produto p2 = new Produto("Impressora", 800.00);
 		Produto p3 = new Produto("Mouse", 80.00);
-
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3)); // Crianda tabela de relação
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1)); 	// Crianda tabela de relação
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3)); // Salvando na tabela produtos
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); // Salvanda na tabela categoria
+		
+		Estado e1 = new Estado("Minas Gerais");
+		Estado e2 = new Estado("São Paulo");
+
+		Cidade c1 = new Cidade("Uberlândia", e1); // Adicionando os estados que as cidades pertecem
+		Cidade c2 = new Cidade("São Paulo", e2);
+		Cidade c3 = new Cidade("Campinas", e2);
+		
+		e1.getCidades().addAll(Arrays.asList(c2, c3)); // Adicionando as cidades associados com os estados
+		e2.getCidades().addAll(Arrays.asList(c1));
+		
+		estadoRepository.saveAll(Arrays.asList(e1, e2)); // Primeiro salva estados!
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+	
 	}
 
 }
---
+++ src/main/java/com/am/aguamarinha/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.am.aguamarinha.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.am.aguamarinha.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> { 
+	
+}
